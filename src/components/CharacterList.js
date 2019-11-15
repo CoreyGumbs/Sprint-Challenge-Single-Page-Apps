7d26799a0a9c5096555a4a3b6b0d329a20a3db40
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-11-15 12:02:57 -0500

    [Add] Added CharacterCard component, route, and link

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
+import {Link} from 'react-router-dom';
+import CharacterCard  from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
@@ -14,14 +16,14 @@ export default function CharacterList() {
     })
     .catch(err => console.log(err));
   }, []);
-
   console.log(characters);
   return (
+
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
       {
         characters.map(character => (
-          <p>{character.name}</p>
+          <CharacterCard character={character} key={character.id}/>
         ))
       }
     </section>
