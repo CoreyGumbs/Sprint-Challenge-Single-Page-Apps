606997a12ccb16836c21a3acfdf2a00a0a31067b
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-11-15 11:35:23 -0500

    [Add] Added WelcomePage, CharacterList & Header Routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,12 @@ export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Characters</li>
+        </ul>
+      </nav>
     </header>
   );
 }
